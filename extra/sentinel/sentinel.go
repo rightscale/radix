14ad6e5c6ba7f61604bb2807081f197a24e69dd2
Dominic Metzger <dominic@rightscale.com>
2014-12-04 18:07:38 -0800

    IN-653 Emptying pools after setting c.alwaysErr
    When c.alwaysErr is set, the client can’t be used anymore. Checking out
    connections from the client will fail and hence we can’t use the opened
    connection anymore. Therefore, we should clean up our connection pools
    to close opened connections.

--- extra/sentinel/sentinel.go
+++ extra/sentinel/sentinel.go
@@ -218,6 +218,9 @@ func (c *Client) spin() {
 
 		case err := <-c.alwaysErrCh:
 			c.alwaysErr = err
+			for _, p := range c.masterPools {
+				p.Empty()
+			}
 
 		case sm := <-c.switchMasterCh:
 			if p, ok := c.masterPools[sm.name]; ok {
