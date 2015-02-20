c27bd96edab39e48b44c6d9aece7ca16e6a68eb7
Brian Picciano <mediocregopher@gmail.com>
2015-02-19 20:21:27 -0500

    make DialTimeout actually use DialTimeout (requested by #53)

--- redis/client.go
+++ redis/client.go
@@ -42,7 +42,7 @@ type request struct {
 // used as the read/write timeout when communicating with redis
 func DialTimeout(network, addr string, timeout time.Duration) (*Client, error) {
 	// establish a connection
-	conn, err := net.Dial(network, addr)
+	conn, err := net.DialTimeout(network, addr, timeout)
 	if err != nil {
 		return nil, err
 	}
