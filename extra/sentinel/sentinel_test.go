f01037075516fb43145c99324f48d9eaa831b482
Dominic Metzger <dominic@rightscale.com>
2015-04-07 15:23:02 -0700

    TSS-1069 Emptying pools after setting c.alwaysErr
    When c.alwaysErr is set, the client can’t be used anymore. Checking out
    connections from the client will fail and hence we can’t use the opened
    connection anymore. Therefore, we should clean up our connection pools
    to close opened connections.

---
+++ extra/sentinel/sentinel_test.go
@@ -0,0 +1,51 @@
+package sentinel
+
+import (
+	"github.com/rightscale/radix/redis"
+	"strings"
+	"testing"
+)
+
+func TestSentinelConnectionError(t *testing.T) {
+	redisMaster := "mymaster"
+
+	sentinelClient := newSentinelClient(t, redisMaster)
+	defer sentinelClient.Close()
+
+	// get the pointer to redisClient in the poll so that we can
+	// check its connection later
+	redisClient := getRedisClient(t, sentinelClient, redisMaster)
+	sentinelClient.PutMaster(redisMaster, redisClient)
+
+	// Simulating a Sentinel connection failure
+	sentinelClient.subClient.Client.Close()
+
+	// The connection in the connection pool should be closed
+	verifyConnectionClosed(t, redisClient)
+}
+
+func newSentinelClient(t *testing.T, redisMaster string) *Client {
+	sentinelClient, err := NewClient("tcp", "localhost:26379", 1, redisMaster)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return sentinelClient
+}
+
+func getRedisClient(t *testing.T, sentinelClient *Client, redisMaster string) *redis.Client {
+	redisClient, err := sentinelClient.GetMaster(redisMaster)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return redisClient
+}
+
+func verifyConnectionClosed(t *testing.T, redisClient *redis.Client) {
+	reply := redisClient.Cmd("PING")
+
+	if reply.Err == nil {
+		t.Fatal("it should have closed connections")
+	} else if !strings.Contains(reply.Err.Error(), "use of closed network connection") {
+		t.Fatal("it should get a connection already closed error but got error: ", reply.Err)
+	}
+}
